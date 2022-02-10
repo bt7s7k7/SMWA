fdbc76c7e70c374fde7ef2ff0d16fb6934b474c3
Branislav Trstenský <bt7s7k7@hotmail.sk>
2022-02-10 12:47:35 +0100

    Fixed CLI dry deploy hack

--- cli/src/app/app.ts
+++ cli/src/app/app.ts
@@ -11,6 +11,44 @@ process.on("SIGINT", function () {
     process.exit(0)
 })
 
+async function deploy(dry: boolean) {
+    const state = await State.createFromConfig()
+    if (!state) return
+
+    const crawler = new fdir()
+        .withRelativePaths()
+        .withSymlinks()
+
+    if (state.config.include) {
+        crawler.glob(...state.config.include)
+    }
+
+    const files = (await crawler.crawl(process.cwd()).withPromise()) as string[]
+    UI.success(`Found ${files.length} files to upload`)
+
+    const zipProgress = UI.indeterminateProgress("Packaging files...")
+    const zip = new AdmZip()
+    const queue: Promise<void>[] = []
+    for (const file of files) {
+        queue.push((async () => {
+            const content = await readFile(file)
+            zip.addFile(file, content)
+        })())
+    }
+    await Promise.all(queue)
+    const zipData = zip.toBuffer()
+    zipProgress.done()
+
+    UI.success(`Upload size: ${fileSize(zipData.byteLength).human("si")}`)
+
+    if (dry) {
+        console.log(files)
+        return
+    }
+
+    await state.deploy(zipData)
+}
+
 void async function () {
     const cli = new CLI("smwa", {
         "deploy init": {
@@ -32,55 +70,34 @@ void async function () {
                 await state.printInfo()
             }
         },
-        "deploy switch service": {
+        "deploy switch": {
             desc: "Menu for selecting a different service",
             callback: async () => {
                 const state = await State.createFromConfig()
                 if (!state) return
 
+                const prevService = state.config.service
+
                 await state.openServiceSelection()
+
+                if (prevService != state.config.service) {
+                    await state.printInfo()
+                }
+
                 await state.save()
+            }
+        },
+        "deploy dry": {
+            desc: "Performs deployment steps without uploading",
+            callback: async () => {
+                await deploy(true)
             }
         },
         deploy: {
             desc: "Deploys files to the server",
-            callback: async ([dry]) => {
-                const state = await State.createFromConfig()
-                if (!state) return
-
-                const crawler = new fdir()
-                    .withRelativePaths()
-                    .withSymlinks()
-
-                if (state.config.include) {
-                    crawler.glob(...state.config.include)
-                }
-
-                const files = (await crawler.crawl(process.cwd()).withPromise()) as string[]
-                UI.success(`Found ${files.length} files to upload`)
-                if (dry) {
-                    console.log(files)
-                    return
-                }
-
-                const zipProgress = UI.indeterminateProgress("Packaging files...")
-                const zip = new AdmZip()
-                const queue: Promise<void>[] = []
-                for (const file of files) {
-                    queue.push((async () => {
-                        const content = await readFile(file)
-                        zip.addFile(file, content)
-                    })())
-                }
-                await Promise.all(queue)
-                const zipData = zip.toBuffer()
-                zipProgress.done()
-
-                UI.success(`Upload size: ${fileSize(zipData.byteLength).human("si")}`)
-                await state.deploy(zipData)
-
-            },
-            argc: NaN
+            callback: async () => {
+                await deploy(false)
+            }
         },
         init: {
             desc: "Creates a SMWA service config",
